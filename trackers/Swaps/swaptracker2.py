89617f85bdfb0f05f4cc7b900a3cb42b06c21589
anabab1999 <anabab@al.insper.edu.br>
2019-07-18 10:54:01 -0300

    terminamos a parna fixed

--- trackers/Swaps/swaptracker2.py
+++ trackers/Swaps/swaptracker2.py
@@ -25,28 +25,29 @@ from pandas.tseries.offsets import DateOffset
         #df['Present Value'] = (df['Payment'] * df['Discount']).round(2)
 #print(df)
 
-def swap(date, busdays, calendartype, numberyear, periodcupons, rate, notional, principal):
-    dc1 = DayCounts(busdays, calendar=calendartype)
-    date1 = pd.to_datetime(date)
-    range = pd.date_range(start=date1, end=date1 + DateOffset(years=numberyear), freq=DateOffset(months=periodcupons))
-    range = dc1.modified_following(range)
 
-    df = pd.DataFrame(data=range[:-1], columns=['Accrual Start'])
-    df['Accrual End'] = range[1:]
+def swap_fixed_leg_cf(today, rate, busdays, calendartype, maturity=10, periodcupons=6, notional=1000000):
+    dc1 = DayCounts(busdays, calendar=calendartype)
+    today = pd.to_datetime(today)
+    date_range = pd.date_range(start=today, end=today + DateOffset(years=maturity), freq=DateOffset(months=periodcupons))
+    date_range = dc1.modified_following(date_range)
+
+    df = pd.DataFrame(data=date_range[:-1], columns=['Accrual Start'])
+    df['Accrual End'] = date_range[1:]
     df['days'] = (df['Accrual End'] - df['Accrual Start']).dt.days
     df['Notional'] = notional
-    df['Coupon']= rate
 
+    df['Principal'] = 0
     lastline = df.tail(1)
-    df.loc[lastline.index,'Principal']=principal
-    #adicionar no payment o principal, em que ele sera somado apenas no ultimo ano.
+    df.loc[lastline.index, 'Principal'] = notional
 
-    df['Payment']= ((df['days']/360)* df['Coupon'] * df['Notional']).round(2)
-    for i in df['Coupon']:
-        df['Discount'] = (df['Coupon'] ** (0.5 * i)).round(2)
-        df['Present Value'] = (df['Payment'] * df['Discount']).round(2)
+    df['Payment'] = (df['days']/360) * rate * df['Notional']
+
+    df['Cash Flow'] = df['Payment'] + df['Principal']
+
     return df
 
-print(swap('2019-04-04','ACT/360','us_trading',5,6,2.336,10000000,10000000))
+
+print(swap_fixed_leg_cf('2019-04-04', 0.02336, 'ACT/360', 'us_trading', 5, 6, 10000000))
 
 
