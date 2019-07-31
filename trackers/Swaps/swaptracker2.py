e90afbbf6a913b28976af9e0cc442969ec74758d
anabab1999 <anabab@al.insper.edu.br>
2019-07-31 12:04:08 -0300

    interpolação

--- trackers/Swaps/swaptracker2.py
+++ trackers/Swaps/swaptracker2.py
@@ -1,33 +1,82 @@
 from calendars import DayCounts
 import pandas as pd
 from pandas.tseries.offsets import DateOffset
+#from bloomberg import BBG
 
-#dc1 = DayCounts('ACT/360', calendar='us_trading')
-#date1 = pd.to_datetime('2019-04-04')
-#range1 = pd.date_range(start=date1, end=date1 + DateOffset(years=5), freq = DateOffset(months=6))
-#range1 = dc1.modified_following(range1)
-#df = pd.DataFrame(data=range1[:-1], columns=['Accrual Start'])
-#df['Accrual End'] = range1[1:]
-#df['days'] = (df['Accrual End'] - df['Accrual Start']).dt.days
-#df['Notional'] = 10000000
-#df['Coupon'] = 0.02336
+# bbg = BBG()
+#
+# tickers = ['S0023Z 1Y BLC2 Curncy',
+#           'S0023Z 1D BLC2 Curncy',
+#           'S0023Z 3M BLC2 Curncy',
+#           'S0023Z 1W BLC2 Curncy',
+#           'S0023Z 10Y BLC2 Curncy',
+#           'S0023Z 1M BLC2 Curncy',
+#           'S0023Z 2Y BLC2 Curncy',
+#           'S0023Z 6M BLC2 Curncy',
+#           'S0023Z 2M BLC2 Curncy',
+#           'S0023Z 5Y BLC2 Curncy',
+#           'S0023Z 4M BLC2 Curncy',
+#           'S0023Z 2D BLC2 Curncy',
+#           'S0023Z 9M BLC2 Curncy',
+#           'S0023Z 3Y BLC2 Curncy',
+#           'S0023Z 4Y BLC2 Curncy',
+#           'S0023Z 50Y BLC2 Curncy',
+#           'S0023Z 12Y BLC2 Curncy',
+#           'S0023Z 18M BLC2 Curncy',
+#           'S0023Z 7Y BLC2 Curncy',
+#           'S0023Z 5M BLC2 Curncy',
+#           'S0023Z 1Y BLC Curncy',
+#           'S0023Z 6Y BLC2 Curncy',
+#           'S0023Z 2W BLC2 Curncy',
+#           'S0023Z 11M BLC2 Curncy',
+#           'S0023Z 15M BLC2 Curncy',
+#           'S0023Z 21M BLC2 Curncy',
+#           'S0023Z 15Y BLC2 Curncy',
+#           'S0023Z 25Y BLC2 Curncy',
+#           'S0023Z 8Y BLC2 Curncy',
+#           'S0023Z 10M BLC2 Curncy',
+#           'S0023Z 1D BLC Curncy',
+#           'S0023Z 20Y BLC2 Curncy',
+#           'S0023Z 33M BLC2 Curncy',
+#           'S0023Z 7M BLC2 Curncy',
+#           'S0023Z 8M BLC2 Curncy',
+#           'S0023Z 11Y BLC2 Curncy',
+#           'S0023Z 14Y BLC2 Curncy',
+#           'S0023Z 18Y BLC2 Curncy',
+#           'S0023Z 19Y BLC2 Curncy',
+#           'S0023Z 23D BLC2 Curncy',
+#           'S0023Z 9Y BLC2 Curncy',
+#           'S0023Z 10Y BLC Curncy',
+#           'S0023Z 10Y ICPL Curncy',
+#           'S0023Z 17M BLC2 Curncy',
+#           'S0023Z 1I BLC2 Curncy',
+#           'S0023Z 1M BLC Curncy',
+#           'S0023Z 22Y BLC2 Curncy',
+#           'S0023Z 28Y BLC2 Curncy',
+#           'S0023Z 2I BLC2 Curncy',
+#           'S0023Z 2W BLC Curncy',
+#           'S0023Z 30Y BLC2 Curncy',
+#           'S0023Z 31Y BLC2 Curncy',
+#           'S0023Z 32Y BLC2 Curncy',
+#           'S0023Z 38Y BLC2 Curncy',
+#           'S0023Z 39Y BLC2 Curncy',
+#           'S0023Z 40Y BLC2 Curncy',
+#           'S0023Z 42D BLC2 Curncy',
+#           'S0023Z 48Y BLC2 Curncy',
+#           'S0023Z 6M BLC Curncy']
+#
+# df_bbg = bbg.fetch_contract_parameter(tickers, "PX_LAST")
+# df_bbg_m = bbg.fetch_contract_parameter(tickers, "MATURITY")
+################  teste ################
+lista = [2.3,2.4,2.5,2.6,2.7]
+df_bbg = lista
+lista2 = ["07/31/2019", "08/31/2019", "09/30/2019", "10/31/2019", "11/30/2019"]
+df_bbg_m = lista2
+df_bbg_m = pd.to_datetime(df_bbg_m)
+interpolate_zero_curve = pd.Series(df_bbg, index = df_bbg_m)
 
-#lastline = df.tail(1)
-
-#df.loc[lastline.index,'Principal']=10000000
-
-#df['Payment'] = ((df['days']/360)* df['Coupon'] * df['Notional']).round(2)
-#contador = 1
-#lista = []
-#for i in df.index:
-   # value_disc = 1/((1+(6/12)*df['Coupon'][i])**contador)
-  #  lista.append(value_disc)
- #   contador += 1
-
-#df["Discount"]=lista
-#df['Present Value'] = (df['Payment'] * df['Discount']).round(2)
-#print(df)
-
+x = interpolate_zero_curve.interpolate(method='cubic', axis=0, limit=None, inplace=False, limit_direction='forward', limit_area=None, downcast=None)
+print(x)
 
 def swap_fixed_leg_pv(today, rate, busdays, calendartype, maturity=10, periodcupons=6, notional=1000000):
     dc1 = DayCounts(busdays, calendar=calendartype)
@@ -48,16 +97,9 @@ def swap_fixed_leg_pv(today, rate, busdays, calendartype, maturity=10, periodcup
 
     df['Cash Flow'] = df['Payment'] + df['Principal']
 
+    df['Cumulative Days'] = df['days'].cumsum()
+    df['Discount'] = 1/(1+rate*(df['Cumulative Days']/360))
 
-    lista = []
-    i=0
-    for contador in df.index + 1:
-        value_disc = 1 / ((1 + (df['days'][i]/ 360)* rate) ** contador)
-        lista.append(value_disc)
-        i+=1
-
-
-    df["Discount"] = lista
     df['Present Value'] = (df['Cash Flow'] * df['Discount'])
 
     return df
@@ -84,14 +126,9 @@ def swap_floating_leg_pv(today, zero_rate, busdays, calendartype, maturity=10, p
 
     df2['Cash Flow'] = df2['Payment'] + df2['Principal']
 
-    lista2 = []
-    i=0
-    for contador in df2.index + 1:
-        value_disc = 1 / ((1 +(df2['days'][i] / 360)  * zero_rate) ** contador)
-        lista2.append(value_disc)
-        i+=1
+    df2['Cumulative Days'] = df2['days'].cumsum()
+    df2['Discount'] = 1 / (1 + zero_rate * (df2['Cumulative Days'] / 360))
 
-    df2["Discount"] = lista2
     df2['Present Value'] = (df2['Cash Flow'] * df2['Discount'])
 
     return df2
